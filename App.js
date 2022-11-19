34c24436a7fbadb02cb68eb15a7b801927ffd6df
Leonardo Leitão <leoleitao27@gmail.comgit config --global user.email leoleitao27@gmail.comgit config --global user.email leoleitao27@gmail.com>
2022-11-18 23:00:37 -0300

    props

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React , {  Component }  from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { render } from "react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod";
 
 
 class App extends Component{
   render(){
+
+    let nome = 'leo';
+
     return(
       <View>
-        <Text>Olá mundo!</Text>
+        <Text>Olá mundo!!</Text>
+        <Text style={{color:'red', fontSize:25, margin:15}}> Leo </Text>
+
+        <Image 
+        source={{uri: 'https://petsdalets.com.br/wp-content/uploads/2022/06/mais-de-um-gato-em-casa-2.jpg'}}
+        style={{height:600, width:300}}
+        />
+        <Text>{nome}</Text>
+
       </View>
     );
   }

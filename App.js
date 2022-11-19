a4d7294831505876de3142b9e35f494c942be784
Leonardo Leitão <leoleitao27@gmail.comgit config --global user.email leoleitao27@gmail.comgit config --global user.email leoleitao27@gmail.com>
2022-11-18 23:21:18 -0300

    props

--- App.js
+++ App.js
@@ -13,13 +13,26 @@ class App extends Component{
         <Text>Olá mundo!!</Text>
         <Text style={{color:'red', fontSize:25, margin:15}}> Leo </Text>
 
-        <Image 
-        source={{uri: 'https://petsdalets.com.br/wp-content/uploads/2022/06/mais-de-um-gato-em-casa-2.jpg'}}
-        style={{height:600, width:300}}
-        />
+        
         <Text>{nome}</Text>
 
+        <Jobs Largura={300} Altura={600} nome1={'leo'}/>
+
       </View>
+    );
+  }
+}
+
+class Jobs extends Component{
+  render(){
+    return(
+      <View>
+        <Image source={{uri: 'https://petsdalets.com.br/wp-content/uploads/2022/06/mais-de-um-gato-em-casa-2.jpg'}}
+        style={{height:this.props.Altura, width:this.props.Largura}}/>
+
+        <Text>{this.props.nome1}</Text>
+      </View>
+
     );
   }
 }

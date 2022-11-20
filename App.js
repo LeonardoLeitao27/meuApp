f3b13b5d3bcff29340557fc6ff4a04c8c69172d7
Leonardo <leoleitao27@gmail.com>
2022-11-20 10:53:14 -0300

    states

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import React , {  Component }  from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Button } from "react-native";
 import { render } from "react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod";
 
 
 class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: 'Leonardo Leitão'
+    };
+
+    this.entrar = this.entrar.bind(this);
+  }
+
+  entrar(nome){
+    this.setState({
+      nome: nome
+    })
+  }
+
+
   render(){
 
-
     return(
-      <View>
-        <Text>Olá mundo!!</Text>
+      <View style={{marginTop:15}}>
+
+        <Button title="Entrar" onPress={() => this.entrar('leozap2')} />
+
+        <Text style={{fontSize:23, color:'red', textAlign:'center'}}>{this.state.nome}</Text>
 
       </View>
     );

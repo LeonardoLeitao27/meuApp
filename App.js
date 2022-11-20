f7ce06582cba14b9c02cc99dd67e736c7b11bd0a
Leonardo <leoleitao27@gmail.com>
2022-11-20 11:05:35 -0300

    grupo de estilos

--- App.js
+++ App.js
@@ -1,40 +1,39 @@
 import React , {  Component }  from "react";
-import { View, Text, Image, Button } from "react-native";
+import { View, Text, Image, Button, StyleSheet } from "react-native";
 import { render } from "react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod";
 
 
 class App extends Component{
   
-  constructor(props){
-    super(props);
-    this.state = {
-      nome: 'Leonardo Leitão'
-    };
-
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar(nome){
-    this.setState({
-      nome: nome
-    })
-  }
-
 
   render(){
 
     return(
-      <View style={{marginTop:15}}>
+      <View style={styles.area}>
 
-        <Button title="Entrar" onPress={() => this.entrar('leozap2')} />
 
-        <Text style={{fontSize:23, color:'red', textAlign:'center'}}>{this.state.nome}</Text>
+        <Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Eu sou texto1</Text>
+        <Text style={styles.alinhaTexto}>Eu sou texto2</Text>
+        <Text>Eu sou texto3</Text>
+        <Text>Eu sou texto4</Text>
 
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  area:{
+    marginTop:40
+  },
+  textoPrincipal:{
+    fontSize: 25,
+    color: 'red'
+  },
+  alinhaTexto:{
+    textAlign: 'center'
+  }
+});
 
 
 export default App;
